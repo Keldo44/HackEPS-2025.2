9b7009ae48988663feed8aeffe8839e9c7948232
Beatriu <120586943+Beatriu@users.noreply.github.com>
2025-11-22 22:27:48 +0100

    Camara

--- Front/prova.js
+++ Front/prova.js
@@ -1,35 +1,19 @@
-import * as THREE from 'https://unpkg.com/three@0.151.2/build/three.module.js';
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const video2 = document.getElementById('video2');
+const constraints = { video: true };
+const constraints2 = { video2: true };
 
-var scene = new THREE.Scene();
-// scene.background = new THREE.Color('skyblue')
+function handleSuccess(stream) {
+  video.srcObject = stream;
+  video.play();
+}
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+function handleError(error) {
+  console.error('Error al acceder a la cámara:', error);
+}
 
-const renderer = new THREE.WebGLRenderer();
+// Accedemos a la cámara
+navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+navigator.mediaDevices.getUserMedia(constraints2).then(handleSuccess).catch(handleError);
 
-// AQUI EL ERROR  -----------v   Tienes 3 "nnn"
-//renderer.setSize( window.innnerWidth, window.innerHeight);
-
-//ASI debería ser:
-renderer.setSize( window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 'skyblue' } );
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-var animate = function(){
-    requestAnimationFrame( animate )
-      
-    //AGREGAR ROTACIÖN
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    
-
-    renderer.render(scene, camera)
-};
-animate(); 
